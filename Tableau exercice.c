af847b5973de544eb23d146c731d27b8c5aadb6a
SamiReg <samireg2002@gmail.com>
2025-09-24 13:42:51 +0100

    Exercices sur les Tableaux en C

--- Tableau exercice.c
+++ Tableau exercice.c
@@ -70,7 +70,7 @@ void challenge4(){
         if (max<array[i])
             max=array[i];
     }
-    printf("Le maximum dans ce tableau est: %i", max);
+    printf("Le maximum dans ce tableau est: %i", max); 
 }
 
 void challenge5(){
@@ -175,9 +175,186 @@ void challenge9(){
 }
 
 void challenge10(){
+    char *array, elem;
+    int n;
+    printf("Combien des elements voulez-vous entrez dans le tableau?\n  ");
+    scanf("%i",&n);
+    
+    array= (char *)malloc(n);
+    while (array==NULL){
+        printf("stockage insuffisant! re-entrez le nombre des elements\n");
+        scanf("%i",&n);
+        array= (char *)malloc(n);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n; i++){
+        getchar();
+        printf("Val%i: ",i+1);
+        scanf("%c",&array[i]);
+    }
+    printf("Donnez l'element a chercher svp: ");
+    getchar();
+    scanf("%c", &elem);
+    int i =0;
+    while (array[i]!=elem && i<n){
+        i++;
+    }
+    if (i==n) printf("l'element n'existe pas dans le tableu");
+    else printf("l'element existe dans le tableu");
+}
+
+void challenge11(){
+    char *array, ocur, remp;
+    int n;
+    printf("Combien des elements voulez-vous entrez dans le tableau?\n  ");
+    scanf("%i",&n);
+    
+    array= (char *)malloc(n);
+    while (array==NULL){
+        printf("stockage insuffisant! re-entrez le nombre des elements\n");
+        scanf("%i",&n);
+        array= (char *)malloc(n);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n; i++){
+        getchar();
+        printf("Val%i: ",i+1);
+        scanf("%c",&array[i]);
+    }
+    printf("Donnez l'element a chercher svp: ");
+    getchar();
+    scanf("%c", &ocur);
+    printf("Donnez le remplacement svp: ");
+    getchar();
+    scanf("%c", &remp);
+    for (int i=0; i<n; i++){
+        if (array[i]==ocur){
+            array[i]=remp;
+            printf("original tableau contenu est: %c  new contenu est: %c\n", ocur, array[i]);
+        }
+        else
+            printf("original tableau contenu est: %c  new contenu est: %c\n", array[i], array[i]);
+    }
 
 }
 
+void challenge12(){
+    int *array, n;
+    printf("Combien des elements voulez-vous entrez dans le tableau?\n  ");
+    scanf("%i",&n);
+    
+    array= (int *)malloc(sizeof(int) * n);
+    while (array==NULL){
+        printf("stockage insuffisant! re-entrez le nombre des elements\n");
+        scanf("%i",&n);
+        array= (int *)malloc(sizeof(int) * n);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n; i++){
+        printf("Val%i: ",i+1);
+        scanf("%i",&array[i]);
+    }
+    printf("les elements pairs sont:");
+    for (int i=0; i<n; i++){
+        if (!(array[i]%2))
+            printf("  %i",array[i]);
+    }
+
+}
+
+void challenge13(){
+    int *array, n;
+    printf("Combien des elements voulez-vous entrez dans le tableau?\n  ");
+    scanf("%i",&n);
+    
+    array= (int *)malloc(sizeof(int) * n);
+    while (array==NULL){
+        printf("stockage insuffisant! re-entrez le nombre des elements\n");
+        scanf("%i",&n);
+        array= (int *)malloc(sizeof(int) * n);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n; i++){
+        printf("Val%i: ",i+1);
+        scanf("%i",&array[i]);
+    }
+    printf("les elements pairs sont:");
+    for (int i=0; i<n; i++){
+        if (array[i]%2)
+            printf("  %i",array[i]);
+    }
+}
+
+void challenge14(){
+    int *array, n, sum;
+    float avg;
+    printf("Combien des elements voulez-vous entrez dans le tableau?\n  ");
+    scanf("%i",&n);
+    
+    array= (int *)malloc(sizeof(int) * n);
+    while (array==NULL){
+        printf("stockage insuffisant! re-entrez le nombre des elements\n");
+        scanf("%i",&n);
+        array= (int *)malloc(sizeof(int) * n);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n; i++){
+        printf("Val%i: ",i+1);
+        scanf("%i",&array[i]);
+        sum+=array[i];
+    }
+    avg=sum/n;
+    printf("la moyenne est %.2f",avg);
+}
+
+void challenge15(){
+    int n1, n2, N;
+    char *array1, *array2, *array3;
+    do{
+        printf("Combien des elements voulez-vous entrez dans le premier tableau?\n  ");
+        scanf("%i",&n1);
+        array1= (char *)malloc(n1);
+        while (array1==NULL){
+            printf("stockage insuffisant! re-entrez le nombre des elements\n");
+            scanf("%i",&n1);
+            array1= (char *)malloc(n1);
+        }
+        printf("Combien des elements voulez-vous entrez dans le deuxieme tableau?\n  ");
+        scanf("%i",&n2);
+        array2= (char *)malloc(n2);
+        while (array2==NULL){
+            printf("stockage insuffisant! re-entrez le nombre des elements\n");
+            scanf("%i",&n2);
+            array2= (char *)malloc(n2);
+        }
+        N=n1+n2;
+        array3= (char *)malloc(N);
+        if (array3==NULL) printf("stockage insuffisant pour le tableau fusionne! re-entrez le nombre des elements\n");
+    } while(array3==NULL);
+    
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n1; i++){
+        getchar();
+        printf("Val%i: ",i+1);
+        scanf("%c",&array1[i]);
+    }
+    printf("Donnez ces valeurs svp\n");
+    for (int i=0; i<n2; i++){
+        getchar();
+        printf("Val%i: ",i+1);
+        scanf("%c",&array2[i]);
+    }
+    int i=0;
+    while (array3[N-1]!=array2[n2-1] || array3[n1-1]!=array1[n1-1]){
+        array3[i]=array1[i];
+        array3[i+n1]=array2[i];
+        i++;
+    }
+    printf("le tableau fusionne\n");
+    for (int i=0; i<N; i++){
+        printf("%c  ",array3[i]);
+    }
+}
 int main(){
     // challenge1();
     // challenge2();
@@ -187,7 +364,12 @@ int main(){
     // challenge6();
     // challenge7();
     // challenge8();
-    challenge9();
-    
+    // challenge9();
+    // challenge10();
+    // challenge11();
+    // challenge12();
+    // challenge13();
+    // challenge14();
+    challenge15();
     return 0;
 }
